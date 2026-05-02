290e3054adc7aae8200bf188385e090d75ef2003
saikaku <grnmnsta@gmail.com>
2026-05-02 11:45:29 +0900

    Initial Commit

---
+++ HongShiTransaction/HongShiTransactionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HongShiTransactionApp.swift
+//  HongShiTransaction
+//
+//  Created by j.lee on 2026/05/02.
+//
+
+import SwiftUI
+
+@main
+struct HongShiTransactionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
